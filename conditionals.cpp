4e0c2cc6ed5a79f1f2faa014654754186414d142
yamcodes123 <75660367+yamcodes123@users.noreply.github.com>
2020-12-08 11:49:41 +0530

    Add files via upload

---
+++ conditionals.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    while(1){
+    cin>>a;
+    if(a>10&&a<20){
+        cout<<"Hello";
+    }
+    else if(a>20&&a<30){
+        cout<<"World!";
+    }
+    else if(a>30&&a<40)
+        cout<<"Hello World!";
+    else
+        cout<<"Welcome";
+    };
+    return 0;
+}
